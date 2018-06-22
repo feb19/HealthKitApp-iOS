9310acc62178904130d291e0e8cf0c57b5b75954
Nobuhiro Takahashi <nt@feb19.jp>
2018-06-22 09:39:24 +0900

    update

--- ActivityRingApp/ViewController.swift
+++ ActivityRingApp/ViewController.swift
@@ -7,12 +7,100 @@
 //
 
 import UIKit
+import HealthKit
+import HealthKitUI
 
 class ViewController: UIViewController {
 
+    var store: HKHealthStore!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        auth()
+    }
+    
+    func auth() {
+        store = HKHealthStore()
+        
+        let objectTypes: Set<HKObjectType> = [
+            HKObjectType.activitySummaryType()
+        ]
+        
+        store.requestAuthorization(toShare: nil, read: objectTypes) { (success, error) in
+            
+            // Authorization request finished, hopefully the user allowed access!
+            
+            self.getSummary()
+        }
+    }
+    
+    func show(activitySummaries: HKActivitySummary) {
+        
+        let ringView = HKActivityRingView(frame: CGRect(x: 0, y: 0, width: 120, height: 120))
+//        let summary = HKActivitySummary()
+        
+        // ムーヴ 
+//        summary.activeEnergyBurnedGoal = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: 200.0)
+//        summary.activeEnergyBurned = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: 150.0)
+//
+        // エクササイズ
+//        summary.appleExerciseTime = HKQuantity(unit: HKUnit.hour(), doubleValue: 0.5)
+//        summary.appleExerciseTimeGoal = HKQuantity(unit: HKUnit.hour(), doubleValue: 1.0)
+//
+        // スタンド
+//        summary.appleStandHours = HKQuantity(unit: HKUnit.count(), doubleValue: 2)
+//        summary.appleStandHoursGoal = HKQuantity(unit: HKUnit.count(), doubleValue: 8)
+        
+        ringView.setActivitySummary(activitySummaries, animated: true)
+        print("move: \(activitySummaries.activeEnergyBurned) / \(activitySummaries.activeEnergyBurnedGoal)")
+        print("exercise: \(activitySummaries.appleExerciseTime) / \(activitySummaries.appleExerciseTimeGoal)")
+        print("stand: \(activitySummaries.appleStandHours) / \(activitySummaries.appleStandHoursGoal)")
+        
+        self.view.addSubview(ringView)
+    }
+    
+    func getSummary() {
+        guard let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian) else {
+            fatalError("*** This should never fail. ***")
+        }
+        
+        let endDate = NSDate()
+        
+        guard let startDate = calendar.date(byAdding: .day, value: -7, to: endDate as Date, options: []) else {
+            fatalError("*** unable to calculate the start date ***")
+        }
+        
+        let units: NSCalendar.Unit = [.day, .month, .year, .era]
+        
+        var startDateComponents = calendar.components(units, from: startDate)
+        startDateComponents.calendar = calendar as Calendar
+        
+        var endDateComponents = calendar.components(units, from: endDate as Date)
+        endDateComponents.calendar = calendar as Calendar
+        
+        
+        // Create the predicate for the query
+        let summariesWithinRange = HKQuery.predicate(forActivitySummariesBetweenStart: startDateComponents, end: endDateComponents)
+        
+        // Build the query
+        let query = HKActivitySummaryQuery(predicate: summariesWithinRange) { (query, summaries, error) -> Void in
+            guard let activitySummaries = summaries else {
+                guard let queryError = error else {
+                    fatalError("*** Did not return a valid error object. ***")
+                }
+                
+                // Handle the error here...
+                
+                return
+            }
+            
+            // Do something with the summaries here...
+            self.show(activitySummaries: activitySummaries.first!)
+        }
+        
+        // Run the query
+        store.execute(query)
     }
 
     override func didReceiveMemoryWarning() {
